1c50fccdd504c91d7e1517bfc56239b12a4a53cc
CDK for Terraform Team <github-team-tf-cdk@hashicorp.com>
2022-07-18 13:08:45 +0000

    chore(release): v0.5.7

--- tfe/jsii/jsii.go
+++ tfe/jsii/jsii.go
@@ -13,7 +13,7 @@ import (
 	cdktf      "github.com/hashicorp/terraform-cdk-go/cdktf/jsii"
 )
 
-//go:embed cdktf-provider-tfe-0.5.6.tgz
+//go:embed cdktf-provider-tfe-0.5.7.tgz
 var tarball []byte
 
 // Initialize loads the necessary packages in the @jsii/kernel to support the enclosing module.
@@ -24,5 +24,5 @@ func Initialize() {
 	constructs.Initialize()
 
 	// Load this library into the kernel
-	_jsii_.Load("@cdktf/provider-tfe", "0.5.6", tarball)
+	_jsii_.Load("@cdktf/provider-tfe", "0.5.7", tarball)
 }
